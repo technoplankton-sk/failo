ba54c5b92ed547ea1cdbfbfcb082fd3fcb85dc67
Technoplankton <novikov666kherson@gmail.com>
2026-08-01 11:47:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
 const multer = require('multer');
-const path = path = require('path');
+const path = require('path');
 const fs = require('fs');
 
 const app = express();
